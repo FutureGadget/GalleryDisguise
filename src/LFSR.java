13b32bd1c1da9f35b2cda0d3f2fce42c56374203
danwoo <danwoopark@gmail.com>
2016-10-16 14:14:56 +0900

    LFSR init modified.

--- src/LFSR.java
+++ src/LFSR.java
@@ -1,16 +1,18 @@
 
 public class LFSR {
 	private static String initial_seed;
+	private static String current_seed;
 	private static int tap_index;
 	public static boolean init(String seed, int tap){
 		//  create LFSR with the given initial seed and tap
 		if (seed.length() - tap < 0) return false;
 		initial_seed = seed;
+		current_seed = seed;
 		tap_index = seed.length() - tap;
 		return true;
 	}
 	public static int step() {
-		return (initial_seed.charAt(0) - '0') ^ (initial_seed.charAt(tap_index) - '0');
+		return (current_seed.charAt(0) - '0') ^ (current_seed.charAt(tap_index) - '0');
 		//  simulate one step and return the least significant (rightmost) bit as 0 or 1
 	}
 	public static int generate(int k) {
