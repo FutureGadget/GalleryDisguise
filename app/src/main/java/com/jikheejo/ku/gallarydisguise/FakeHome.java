4c27825ec48bc4d9d96635bae094e74647058f61
daseob <daseob13@hanmail.net>
2016-11-20 14:20:02 +0900

    added fake home

---
+++ app/src/main/java/com/jikheejo/ku/gallarydisguise/FakeHome.java
@@ -0,0 +1,13 @@
+package com.jikheejo.ku.gallarydisguise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FakeHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_home);
+    }
+}
