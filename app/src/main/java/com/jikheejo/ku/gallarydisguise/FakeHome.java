2f9ec01c8ba5fdb894fde18e9ed3cdd957f9467c
daseob <daseob13@hanmail.net>
2016-11-20 15:10:41 +0900

    added fake home

--- app/src/main/java/com/jikheejo/ku/gallarydisguise/FakeHome.java
+++ app/src/main/java/com/jikheejo/ku/gallarydisguise/FakeHome.java
@@ -1,7 +1,10 @@
 package com.jikheejo.ku.gallarydisguise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FakeHome extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class FakeHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_home);
+        Button b = (Button)findViewById(R.id.logobutton);
+        b.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent inten = new Intent(FakeHome.this, HomeScreenActivity.class);
+                startActivity(inten);
+                return false;
+            }
+        });
     }
+
+
 }
