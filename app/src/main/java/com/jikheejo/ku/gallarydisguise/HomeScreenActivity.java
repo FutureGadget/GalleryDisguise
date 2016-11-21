14d1f0b23f5225bfe1e95156de753e6ec552c5e4
daseob <daseob13@hanmail.net>
2016-11-21 19:49:52 +0900

    Sever img download

--- app/src/main/java/com/jikheejo/ku/gallarydisguise/HomeScreenActivity.java
+++ app/src/main/java/com/jikheejo/ku/gallarydisguise/HomeScreenActivity.java
@@ -37,6 +37,11 @@ public class HomeScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
+        //list setting
+        mDirRecyclerView = (RecyclerView)findViewById(R.id.dirRecyclerView);
+        mDirRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        updateUI();
+
         //back button setting
         backButtonPress = new BackButtonPress(this);
 
@@ -68,10 +73,7 @@ public class HomeScreenActivity extends Activity {
             }
         });
 
-        //list setting
-        mDirRecyclerView = (RecyclerView)findViewById(R.id.dirRecyclerView);
-        mDirRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        updateUI();
+
     }
 
     //back button run
