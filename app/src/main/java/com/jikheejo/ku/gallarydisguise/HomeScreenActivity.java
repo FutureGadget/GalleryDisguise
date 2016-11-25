f5357d88b8117dc552af1f12af7bdf9cbe0cfa30
danwoo <danwoopark@gmail.com>
2016-11-25 16:08:50 +0900

    android permission problem solved

--- app/src/main/java/com/jikheejo/ku/gallarydisguise/HomeScreenActivity.java
+++ app/src/main/java/com/jikheejo/ku/gallarydisguise/HomeScreenActivity.java
@@ -1,15 +1,19 @@
 package com.jikheejo.ku.gallarydisguise;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -55,6 +59,7 @@ public class HomeScreenActivity extends AppCompatActivity {
     private RecyclerView mDirRecyclerView;
     private DirListAdapter mAdapter;
     private BackButtonPress backButtonPress;
+    private final int MY_PERMISSIONS_READ_WRITE_EXTERNAL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +70,33 @@ public class HomeScreenActivity extends AppCompatActivity {
         mDirRecyclerView = (RecyclerView) findViewById(R.id.dirRecyclerView);
         mDirRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        updateUI();
+        // Request permission
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(HomeScreenActivity.this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(HomeScreenActivity.this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(HomeScreenActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_READ_WRITE_EXTERNAL);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
 
         //back button setting
         backButtonPress = new BackButtonPress(this);
@@ -127,6 +158,30 @@ public class HomeScreenActivity extends AppCompatActivity {
 //                dialog.show();
 //            }
 //        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_READ_WRITE_EXTERNAL: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    updateUI();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
