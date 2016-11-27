d674ea0f43f209d5d7286fc7fa3bc7a6d2777db5
danwoo <danwoopark@gmail.com>
2016-11-27 23:23:59 +0900

    removed unused libraries

--- app/src/main/java/com/jikheejo/ku/gallarydisguise/FakeHome.java
+++ app/src/main/java/com/jikheejo/ku/gallarydisguise/FakeHome.java
@@ -1,17 +1,11 @@
 package com.jikheejo.ku.gallarydisguise;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.HapticFeedbackConstants;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.widget.Button;
 import android.widget.ImageView;
 
